87505e4154f6eb41d45d73bf5db088d8ed471316
Raul Rosenlöcher <git@home>
2020-05-18 22:16:08 +0200

    new device Nodon SIN-2-2

---
+++ src/device/nodon_sin22_actor.py
@@ -0,0 +1,104 @@
+import logging
+from collections import namedtuple
+
+from enocean.protocol.constants import PACKET
+from enocean.protocol.packet import Packet
+
+from src.config import Config
+from src.device.conf_device_key import ConfDeviceKey
+from src.device.rocker_actor import RockerActor, StateValue, ActorCommand
+from src.enocean_connector import EnoceanMessage
+from src.tools import Tools
+
+_Notification = namedtuple("_Notification", ["channel", "switch_state"])
+
+
+class NodonSin22Actor(RockerActor):
+    """Actor for Nodon SIN-2-2-01"""
+
+    DEFAULT_ENOCEAN_RORG = 0xd2
+    DEFAULT_ENOCEAN_FUNC = 0x01
+    DEFAULT_ENOCEAN_TYPE = 0x01
+    # type should be 0x02, but it's not available within "enocean" lib
+
+    DEFAULT_ENOCEAN_DIRECTION = None
+    DEFAULT_ENOCEAN_COMMAND = None  # 0x01
+
+    def __init__(self, name):
+        super().__init__(name)
+
+        self._time_between_rocker_commands = 0.2
+
+        # default config values
+        self._enocean_rorg = self.DEFAULT_ENOCEAN_RORG
+        self._enocean_func = self.DEFAULT_ENOCEAN_FUNC
+        self._enocean_type = self.DEFAULT_ENOCEAN_TYPE
+        self._enocean_direction = self.DEFAULT_ENOCEAN_DIRECTION
+        self._enocean_command = self.DEFAULT_ENOCEAN_COMMAND
+
+        self._actor_channel = None
+
+    def set_config(self, config):
+        super().set_config(config)
+
+        self._actor_channel = Config.get_int(config, ConfDeviceKey.ACTOR_CHANNEL)
+        if self._actor_channel is None:
+            raise ValueError(f"No configuration for '{ConfDeviceKey.ACTOR_CHANNEL.value}'!")
+
+    def process_enocean_message(self, message: EnoceanMessage):
+
+        packet = message.payload  # type: Packet
+        if packet.packet_type != PACKET.RADIO:
+            self._logger.debug("skipped packet with packet_type=%s", Tools.packet_type_text(packet.rorg))
+            return
+        if packet.rorg != self._enocean_rorg:
+            self._logger.debug("skipped packet with rorg=%s", hex(packet.rorg))
+            return
+
+        data = self._extract_packet(packet)
+        self._logger.debug("proceed_enocean - got: %s", data)
+
+        notification = self.extract_notification(data)
+        if notification.channel != self._actor_channel:
+            self._logger.debug("skip channel (%s, awaiting=%s)", notification.channel, self._actor_channel)
+            return
+
+        rssi = packet.dBm  # if hasattr(packet, "dBm") else None
+
+        if notification.switch_state == StateValue.ERROR and self._logger.isEnabledFor(logging.DEBUG):
+            # write ascii representation to reproduce in tests
+            self._logger.debug("process_enocean_message - pickled error packet:\n%s", Tools.pickle_packet(packet))
+
+        message = self._create_message(notification.switch_state, None, rssi)
+        self._publish_mqtt(message)
+
+    @classmethod
+    def extract_notification(cls, data):
+        # {'PF': 0, 'PFD': 0, 'CMD': 4, 'OC': 0, 'EL': 3, 'IO': 1, 'LC': 1, 'OV': 0}
+        # {'PF': 0, 'PFD': 0, 'CMD': 4, 'OC': 0, 'EL': 3, 'IO': 0, 'LC': 1, 'OV': 100}
+        # {'PF': 0, 'PFD': 0, 'CMD': 4, 'OC': 0, 'EL': 3, 'IO': 1, 'LC': 1, 'OV': 100}
+        # {'PF': 0, 'PFD': 0, 'CMD': 4, 'OC': 0, 'EL': 3, 'IO': 0, 'LC': 1, 'OV': 0}
+        value = int(data.get("OV"))
+
+        if value == 0:
+            switch_state = StateValue.OFF
+        elif 0 < value <= 100:
+            switch_state = StateValue.ON
+        else:
+            switch_state = StateValue.ERROR
+
+        return _Notification(channel=int(data.get("IO")), switch_state=switch_state)
+
+    def get_teach_print_message(self):
+        return "Nodon SIN-2-2-01: 1 channel per configured device (no parameters)!"
+
+    def send_teach_telegram(self, cli_arg):
+        command = ActorCommand.ON
+
+        if cli_arg:
+            try:
+                command = self.extract_actor_command(cli_arg)
+            except ValueError:
+                raise ValueError("could not interprete teach argument ({})!".format(cli_arg))
+
+        self._execute_actor_command(command, learn=False)
---
+++ test/device/test_nodon_sin22_actor.py
@@ -0,0 +1,87 @@
+import unittest
+
+from src.device.rocker_actor import StateValue
+from src.device.nodon_sin22_actor import NodonSin22Actor
+from src.tools import Tools
+from test.setup_test import SetupTest
+
+
+class _MockDevice(NodonSin22Actor):
+
+    def __init__(self):
+        self.now = None
+
+        super().__init__("mock")
+
+        self._enocean_id = 0xffffffff
+
+        self.messages = []
+        self.packets = []
+
+    def _now(self):
+        return self.now
+
+    def _publish_mqtt(self, message: str, mqtt_channel: str = None):
+        self.messages.append(message)
+
+    def _send_enocean_packet(self, packet, delay=0):
+        self.packets.append(packet)
+
+
+_PACKET_1_OFF = """gAN9cQAoWAsAAABwYWNrZXRfdHlwZXEBSwFYBAAAAGRhdGFxAl1xAyhL0ksES2FLgEsFSxlLUEuo
+SwBlWAgAAABvcHRpb25hbHEEXXEFKEsAS/9L/0v/S/9LMUsAZXUu"""
+
+_PACKET_0_ON = """gAN9cQAoWAsAAABwYWNrZXRfdHlwZXEBSwFYBAAAAGRhdGFxAl1xAyhL0ksES2BL5EsFSxlLUEuo
+SwBlWAgAAABvcHRpb25hbHEEXXEFKEsAS/9L/0v/S/9LMEsAZXUu"""
+
+_PACKET_1_ON = """gAN9cQAoWAsAAABwYWNrZXRfdHlwZXEBSwFYBAAAAGRhdGFxAl1xAyhL0ksES2FL5EsFSxlLUEuo
+SwBlWAgAAABvcHRpb25hbHEEXXEFKEsAS/9L/0v/S/9LN0sAZXUu"""
+
+_PACKET_0_OFF = """gAN9cQAoWAsAAABwYWNrZXRfdHlwZXEBSwFYBAAAAGRhdGFxAl1xAyhL0ksES2BLgEsFSxlLUEuo
+SwBlWAgAAABvcHRpb25hbHEEXXEFKEsAS/9L/0v/S/9LOksAZXUu"""
+
+
+class TestNodonSin2Actor(unittest.TestCase):
+
+    def setUp(self):
+        SetupTest.set_dummy_sender_id()
+
+    def test_extract_0_on(self):
+        packet = Tools.unpickle_packet(_PACKET_0_ON)
+
+        device = _MockDevice()
+        data = device._extract_packet(packet)
+
+        notification = device.extract_notification(data)
+        self.assertEqual(notification.channel, 0)
+        self.assertEqual(notification.switch_state, StateValue.ON)
+
+    def test_extract_0_off(self):
+        packet = Tools.unpickle_packet(_PACKET_0_OFF)
+
+        device = _MockDevice()
+        data = device._extract_packet(packet)
+
+        notification = device.extract_notification(data)
+        self.assertEqual(notification.channel, 0)
+        self.assertEqual(notification.switch_state, StateValue.OFF)
+
+    def test_extract_1_on(self):
+        packet = Tools.unpickle_packet(_PACKET_1_ON)
+
+        device = _MockDevice()
+        data = device._extract_packet(packet)
+
+        notification = device.extract_notification(data)
+        self.assertEqual(notification.channel, 1)
+        self.assertEqual(notification.switch_state, StateValue.ON)
+
+    def test_extract_1_off(self):
+        packet = Tools.unpickle_packet(_PACKET_1_OFF)
+
+        device = _MockDevice()
+        data = device._extract_packet(packet)
+
+        notification = device.extract_notification(data)
+        self.assertEqual(notification.channel, 1)
+        self.assertEqual(notification.switch_state, StateValue.OFF)
